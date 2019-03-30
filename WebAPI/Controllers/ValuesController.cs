c3a6560fca204c1ac546ffa57fc4d674c6266067
AMR <amr@home.local>
2019-03-30 18:10:11 +0200

    Add Web_API

---
+++ WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Amr", "Mohamed" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            
+            if (id==1)
+            {
+                return "Amr Mohamed Abu-Alfetoh";
+            }
+            else
+            {
+                return "Ahmed Mohamed Abu-Alfetoh";
+            }
+
+
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
