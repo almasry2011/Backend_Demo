e4e8664e2fefc11c9dee62ff610ea788573cb3ae
AMR <amr@home.local>
2019-03-30 18:49:04 +0200

    Consume Web_API_console_App

---
+++ ConsoleWebAPI_Consume/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWebAPI_Consume
+{
+    class Program
+    {
+         
+        static  void Main(string[] args)
+        {
+             
+            Program p = new Program();
+            p.Getall();
+            Console.ReadKey();
+        }
+
+        public  async void Getall()
+        {
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:2061/customers");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //GET Method  
+                HttpResponseMessage response = await client.GetAsync("api/products/5");
+                if (response.IsSuccessStatusCode)
+                {
+                    var product = await response.Content.ReadAsAsync<product0>();
+                    Console.WriteLine("ProductName:{0}\tUnitPrice:{1}", product.ProductName, product.UnitPrice);
+                    //Console.WriteLine("No of Employee in Department: {0}", );
+                }
+                else
+                {
+                    Console.WriteLine("Internal server Error");
+                }
+            }
+
+        }
+
+    }
+}
